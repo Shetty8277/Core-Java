12d56524e906fdbcf953a7a7f0caee9b857ec9ab
Shetty8277 <ravishetty2703@gmail.com>
2025-04-02 14:21:18 +0530

    intial commit

--- CoreJava/src/Ball.java
+++ CoreJava/src/Ball.java
@@ -2,6 +2,8 @@ public class Ball {
     public static void main(String[] args) {
         System.out.println("The class Created");
         System.out.println("The class Created");
+        System.out.println("The class Created");
+
 
     }
 }
