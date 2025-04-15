afb3dcf03623f56f5d16a833f0ea1121318ad05c
Shetty8277 <ravishetty2703@gmail.com>
2025-04-16 01:29:26 +0530

    inital commit

---
+++ HundredExample/src/assignment/TeaShop.java
@@ -0,0 +1,316 @@
+package assignment;
+
+public class TeaShop {
+
+    public void debit(Account account) {
+        if (account != null) {
+            account.credentials();
+
+            if (account instanceof Account1) {
+                Account1 account1 = (Account1) account;
+                account1.uniqueMethod1();
+            } else if (account instanceof Account2) {
+                Account2 account2 = (Account2) account;
+                account2.uniqueMethod2();
+            } else if (account instanceof Account3) {
+                Account3 account3 = (Account3) account;
+                account3.uniqueMethod3();
+            } else if (account instanceof Account4) {
+                Account4 account4 = (Account4) account;
+                account4.uniqueMethod4();
+            } else if (account instanceof Account5) {
+                Account5 account5 = (Account5) account;
+                account5.uniqueMethod5();
+            } else if (account instanceof Account6) {
+                Account6 account6 = (Account6) account;
+                account6.uniqueMethod6();
+            } else if (account instanceof Account7) {
+                Account7 account7 = (Account7) account;
+                account7.uniqueMethod7();
+            } else if (account instanceof Account8) {
+                Account8 account8 = (Account8) account;
+                account8.uniqueMethod8();
+            } else if (account instanceof Account9) {
+                Account9 account9 = (Account9) account;
+                account9.uniqueMethod9();
+            } else if (account instanceof Account10) {
+                Account10 account10 = (Account10) account;
+                account10.uniqueMethod10();
+            } else if (account instanceof Account11) {
+                Account11 account11 = (Account11) account;
+                account11.uniqueMethod11();
+            } else if (account instanceof Account12) {
+                Account12 account12 = (Account12) account;
+                account12.uniqueMethod12();
+            } else if (account instanceof Account13) {
+                Account13 account13 = (Account13) account;
+                account13.uniqueMethod13();
+            } else if (account instanceof Account14) {
+                Account14 account14 = (Account14) account;
+                account14.uniqueMethod14();
+            } else if (account instanceof Account15) {
+                Account15 account15 = (Account15) account;
+                account15.uniqueMethod15();
+            } else if (account instanceof Account16) {
+                Account16 account16 = (Account16) account;
+                account16.uniqueMethod16();
+            } else if (account instanceof Account17) {
+                Account17 account17 = (Account17) account;
+                account17.uniqueMethod17();
+            } else if (account instanceof Account18) {
+                Account18 account18 = (Account18) account;
+                account18.uniqueMethod18();
+            } else if (account instanceof Account19) {
+                Account19 account19 = (Account19) account;
+                account19.uniqueMethod19();
+            } else if (account instanceof Account20) {
+                Account20 account20 = (Account20) account;
+                account20.uniqueMethod20();
+            } else if (account instanceof Account21) {
+                Account21 account21 = (Account21) account;
+                account21.uniqueMethod21();
+            } else if (account instanceof Account22) {
+                Account22 account22 = (Account22) account;
+                account22.uniqueMethod22();
+            } else if (account instanceof Account23) {
+                Account23 account23 = (Account23) account;
+                account23.uniqueMethod23();
+            } else if (account instanceof Account24) {
+                Account24 account24 = (Account24) account;
+                account24.uniqueMethod24();
+            } else if (account instanceof Account25) {
+                Account25 account25 = (Account25) account;
+                account25.uniqueMethod25();
+            } else if (account instanceof Account26) {
+                Account26 account26 = (Account26) account;
+                account26.uniqueMethod26();
+            } else if (account instanceof Account27) {
+                Account27 account27 = (Account27) account;
+                account27.uniqueMethod27();
+            } else if (account instanceof Account28) {
+                Account28 account28 = (Account28) account;
+                account28.uniqueMethod28();
+            } else if (account instanceof Account29) {
+                Account29 account29 = (Account29) account;
+                account29.uniqueMethod29();
+            } else if (account instanceof Account30) {
+                Account30 account30 = (Account30) account;
+                account30.uniqueMethod30();
+            } else if (account instanceof Account31) {
+                Account31 account31 = (Account31) account;
+                account31.uniqueMethod31();
+            } else if (account instanceof Account32) {
+                Account32 account32 = (Account32) account;
+                account32.uniqueMethod32();
+            } else if (account instanceof Account33) {
+                Account33 account33 = (Account33) account;
+                account33.uniqueMethod33();
+            } else if (account instanceof Account34) {
+                Account34 account34 = (Account34) account;
+                account34.uniqueMethod34();
+            } else if (account instanceof Account35) {
+                Account35 account35 = (Account35) account;
+                account35.uniqueMethod35();
+            } else if (account instanceof Account36) {
+                Account36 account36 = (Account36) account;
+                account36.uniqueMethod36();
+            } else if (account instanceof Account37) {
+                Account37 account37 = (Account37) account;
+                account37.uniqueMethod37();
+            } else if (account instanceof Account38) {
+                Account38 account38 = (Account38) account;
+                account38.uniqueMethod38();
+            } else if (account instanceof Account39) {
+                Account39 account39 = (Account39) account;
+                account39.uniqueMethod39();
+            } else if (account instanceof Account40) {
+                Account40 account40 = (Account40) account;
+                account40.uniqueMethod40();
+            } else if (account instanceof Account41) {
+                Account41 account41 = (Account41) account;
+                account41.uniqueMethod41();
+            } else if (account instanceof Account42) {
+                Account42 account42 = (Account42) account;
+                account42.uniqueMethod42();
+            } else if (account instanceof Account43) {
+                Account43 account43 = (Account43) account;
+                account43.uniqueMethod43();
+            } else if (account instanceof Account44) {
+                Account44 account44 = (Account44) account;
+                account44.uniqueMethod44();
+            } else if (account instanceof Account45) {
+                Account45 account45 = (Account45) account;
+                account45.uniqueMethod45();
+            } else if (account instanceof Account46) {
+                Account46 account46 = (Account46) account;
+                account46.uniqueMethod46();
+            } else if (account instanceof Account47) {
+                Account47 account47 = (Account47) account;
+                account47.uniqueMethod47();
+            } else if (account instanceof Account48) {
+                Account48 account48 = (Account48) account;
+                account48.uniqueMethod48();
+            } else if (account instanceof Account49) {
+                Account49 account49 = (Account49) account;
+                account49.uniqueMethod49();
+            } else if (account instanceof Account50) {
+                Account50 account50 = (Account50) account;
+                account50.uniqueMethod50();
+            } else if (account instanceof Account51) {
+                Account51 account51 = (Account51) account;
+                account51.uniqueMethod51();
+            } else if (account instanceof Account52) {
+                Account52 account52 = (Account52) account;
+                account52.uniqueMethod52();
+            } else if (account instanceof Account53) {
+                Account53 account53 = (Account53) account;
+                account53.uniqueMethod53();
+            } else if (account instanceof Account54) {
+                Account54 account54 = (Account54) account;
+                account54.uniqueMethod54();
+            } else if (account instanceof Account55) {
+                Account55 account55 = (Account55) account;
+                account55.uniqueMethod55();
+            } else if (account instanceof Account56) {
+                Account56 account56 = (Account56) account;
+                account56.uniqueMethod56();
+            } else if (account instanceof Account57) {
+                Account57 account57 = (Account57) account;
+                account57.uniqueMethod57();
+            } else if (account instanceof Account58) {
+                Account58 account58 = (Account58) account;
+                account58.uniqueMethod58();
+            } else if (account instanceof Account59) {
+                Account59 account59 = (Account59) account;
+                account59.uniqueMethod59();
+            } else if (account instanceof Account60) {
+                Account60 account60 = (Account60) account;
+                account60.uniqueMethod60();
+            } else if (account instanceof Account61) {
+                Account61 account61 = (Account61) account;
+                account61.uniqueMethod61();
+            } else if (account instanceof Account62) {
+                Account62 account62 = (Account62) account;
+                account62.uniqueMethod62();
+            } else if (account instanceof Account63) {
+                Account63 account63 = (Account63) account;
+                account63.uniqueMethod63();
+            } else if (account instanceof Account64) {
+                Account64 account64 = (Account64) account;
+                account64.uniqueMethod64();
+            } else if (account instanceof Account65) {
+                Account65 account65 = (Account65) account;
+                account65.uniqueMethod65();
+            } else if (account instanceof Account66) {
+                Account66 account66 = (Account66) account;
+                account66.uniqueMethod66();
+            } else if (account instanceof Account67) {
+                Account67 account67 = (Account67) account;
+                account67.uniqueMethod67();
+            } else if (account instanceof Account68) {
+                Account68 account68 = (Account68) account;
+                account68.uniqueMethod68();
+            } else if (account instanceof Account69) {
+                Account69 account69 = (Account69) account;
+                account69.uniqueMethod69();
+            } else if (account instanceof Account70) {
+                Account70 account70 = (Account70) account;
+                account70.uniqueMethod70();
+            } else if (account instanceof Account71) {
+                Account71 account71 = (Account71) account;
+                account71.uniqueMethod71();
+            } else if (account instanceof Account72) {
+                Account72 account72 = (Account72) account;
+                account72.uniqueMethod72();
+            } else if (account instanceof Account73) {
+                Account73 account73 = (Account73) account;
+                account73.uniqueMethod73();
+            } else if (account instanceof Account74) {
+                Account74 account74 = (Account74) account;
+                account74.uniqueMethod74();
+            } else if (account instanceof Account75) {
+                Account75 account75 = (Account75) account;
+                account75.uniqueMethod75();
+            } else if (account instanceof Account76) {
+                Account76 account76 = (Account76) account;
+                account76.uniqueMethod76();
+            } else if (account instanceof Account77) {
+                Account77 account77 = (Account77) account;
+                account77.uniqueMethod77();
+            } else if (account instanceof Account78) {
+                Account78 account78 = (Account78) account;
+                account78.uniqueMethod78();
+            } else if (account instanceof Account79) {
+                Account79 account79 = (Account79) account;
+                account79.uniqueMethod79();
+            } else if (account instanceof Account80) {
+                Account80 account80 = (Account80) account;
+                account80.uniqueMethod80();
+            } else if (account instanceof Account81) {
+                Account81 account81 = (Account81) account;
+                account81.uniqueMethod81();
+            } else if (account instanceof Account82) {
+                Account82 account82 = (Account82) account;
+                account82.uniqueMethod82();
+            } else if (account instanceof Account83) {
+                Account83 account83 = (Account83) account;
+                account83.uniqueMethod83();
+            } else if (account instanceof Account84) {
+                Account84 account84 = (Account84) account;
+                account84.uniqueMethod84();
+            } else if (account instanceof Account85) {
+                Account85 account85 = (Account85) account;
+                account85.uniqueMethod85();
+            } else if (account instanceof Account86) {
+                Account86 account86 = (Account86) account;
+                account86.uniqueMethod86();
+            } else if (account instanceof Account87) {
+                Account87 account87 = (Account87) account;
+                account87.uniqueMethod87();
+            } else if (account instanceof Account88) {
+                Account88 account88 = (Account88) account;
+                account88.uniqueMethod88();
+            } else if (account instanceof Account89) {
+                Account89 account89 = (Account89) account;
+                account89.uniqueMethod89();
+            } else if (account instanceof Account90) {
+                Account90 account90 = (Account90) account;
+                account90.uniqueMethod90();
+            } else if (account instanceof Account91) {
+                Account91 account91 = (Account91) account;
+                account91.uniqueMethod91();
+            } else if (account instanceof Account92) {
+                Account92 account92 = (Account92) account;
+                account92.uniqueMethod92();
+            } else if (account instanceof Account93) {
+                Account93 account93 = (Account93) account;
+                account93.uniqueMethod93();
+            } else if (account instanceof Account94) {
+                Account94 account94 = (Account94) account;
+                account94.uniqueMethod94();
+            } else if (account instanceof Account95) {
+                Account95 account95 = (Account95) account;
+                account95.uniqueMethod95();
+            } else if (account instanceof Account96) {
+                Account96 account96 = (Account96) account;
+                account96.uniqueMethod96();
+            } else if (account instanceof Account97) {
+                Account97 account97 = (Account97) account;
+                account97.uniqueMethod97();
+            } else if (account instanceof Account98) {
+                Account98 account98 = (Account98) account;
+                account98.uniqueMethod98();
+            } else if (account instanceof Account99) {
+                Account99 account99 = (Account99) account;
+                account99.uniqueMethod99();
+            } else if (account instanceof Account100) {
+                Account100 account100 = (Account100) account;
+                account100.uniqueMethod100();
+            } else {
+                System.err.println("account is not of recognized type");
+            }
+        } else {
+            System.err.println("account is null..");
+        }
+    }
+}
